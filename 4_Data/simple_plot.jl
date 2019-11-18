8cb16b683028bc0c2d14f7a8f14eae16516db294
Jan Slany <jan.slany@mail.muni.cz>
2019-11-18 22:22:45 +0100

    Fix plot colors and annotations in task 1

--- 4_Data/simple_plot.jl
+++ 4_Data/simple_plot.jl
@@ -2,11 +2,11 @@ using Plots
 using LaTeXStrings
 pyplot()
 
-colors =  [:red,:green,:blue,:magenta,:black,:yellow,:cyan]
+colors =  [:red,:orange,:gold,:green,:blue,:indigo,:violet]
 x = range(0, 2*pi; length=1000)
 
 plot()
 for n in 1:5
-    plot!(x,sin.(n*x)./x,c=colors[n],label = latexstring("x^{$(n)}"))
+    plot!(x,sin.(n*x)./x,c=colors[n],label = latexstring("sinc($(n)x)"))
 end
 Plots.gui()

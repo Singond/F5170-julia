fb42c87570d5aa0e0782a37797efb1b9470fcbe8
Jan Slany <jan.slany@ais-servis.cz>
2020-01-03 19:02:11 +0100

    3: Clarify code

--- 3_Motion/motion.jl
+++ 3_Motion/motion.jl
@@ -4,15 +4,29 @@ pyplot()
 using DifferentialEquations
 using LinearAlgebra
 
-function derivs!(dy,y,p,t)
-    dy[1:3] = y[4:6]
-    dy[4:6] = p[1]/p[2]*(p[3] + y[4:6] × p[4])
+# Parameters of the problem
+struct Params
+	charge              # Particle charge
+	mass                # Particle mass
+	elfield             # Electric field acting on particle
+	magfield            # Magnetic field acting on particle
 end
 
+function derivs!(dy, y, p::Params, t)
+	# y[1:3] is position; y[4:6] is velocity
+	dy[1:3] = y[4:6]
+	dy[4:6] = p.charge/p.mass * (p.elfield + y[4:6] × p.magfield)
+end
+
+const elemcharge = 1.602E-19      # Elementary charge [C]
+const electronmass = 9.109E-31    # Mass of an electron [kg]
+
+elfield = [0.0, 1, 0]
+magfield = [0.0, 0, 1]
+p = Params(elemcharge, electronmass, elfield, magfield)
 y0 = [0.0,0,0,0,0,0]
-p = [1.0,1.0,[0.0,1,0],[0.0,0,1]]
-tspan = (0.0,10.0)
-prob = ODEProblem(derivs!,y0,tspan,p)
+tspan = (0.0, 1.0)
+prob = ODEProblem(derivs!, y0, tspan, p)
 
 sol = solve(prob)
 

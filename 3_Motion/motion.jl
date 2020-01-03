45ae96468c502bcf8ee93c07a0ff829d6937ced3
Jan Slany <jan.slany@mail.muni.cz>
2020-01-04 00:28:49 +0100

    3: Customize initial conditions

--- 3_Motion/motion.jl
+++ 3_Motion/motion.jl
@@ -22,15 +22,18 @@ const elemcharge = 1.602E-19      # Elementary charge [C]
 const electronmass = 9.109E-31    # Mass of an electron [kg]
 
 elfield = [0.0, 1, 0]
-magfield = [0.0, 0, 1]
+magfield = [0.0, 0, 2]
+initposition = [0.0, 0, 0]
+initvelocity = [-1.0, 0, 0]
+
 p = Params(elemcharge, electronmass, elfield, magfield)
-y0 = [0.0,0,0,0,0,0]
-tspan = (0.0, 1.0)
+y0 = [initposition initvelocity]
+tspan = (0.0, 1.0E-10)
 prob = ODEProblem(derivs!, y0, tspan, p)
 
 sol = solve(prob)
 
-plot(sol,vars=(1,2))
+plot(sol, vars=(1,2))
 
 #t = range(tspan[1],length=1000,stop=tspan[2])
 #a = sol(t)'[:,1:3]
@@ -40,4 +43,3 @@ plot(sol,vars=(1,2))
 #z = a[:,3]
 #
 #plot(x,y)
-
